4886c57084aa06b8df0723d0532e21c5a453dc69
Vinutha <vinuthareddy47@gmail.com>
2024-02-14 15:02:11 +0530

    sync

---
+++ c/DSA/Lab/hash.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 5
+int a[max],count=0;
+void display(){
+    int i;
+    printf("\n Hash table contents :\n");\
+    printf("Index\tData\n");
+    for(i=0;i<max;i++)
+        printf("\n%d\t%d\n",i,a[i]);
+}
+void linear_probe(int index,int num){
+    int i;
+    if(count==max){
+        printf("Hash table is full\n");
+        return;
+    }
+    else if(a[index]==-1){
+        a[index]=num;
+        count++;
+    }
+    else{
+        printf("\n Collision detected");
+        printf("\n Collision resolved through linear probe");
+        for(i=index+1;i<max;i++){
+            if(a[i]==-1){
+                a[i]=num;
+                count++;
+                return;
+            }
+        }
+        for(i=0;i<index;i++){
+            if(a[i]==-1){
+                a[i]=num;
+                count++;
+                return;
+            }
+        }
+    }
+}
+int main(){
+    int num,index,input,i;
+    for(i=0;i<max;i++)
+        a[i]=-1;
+    do{
+        printf("\n Enter the number : ");
+        scanf("%d",&num);
+        index=-num%max;
+        linear_probe(index,num);
+        display();
+        printf("\n Enter 1 to continue : ");
+        scanf("%d",&input);
+    }while(input==1);
+    return 0;
+}
+/*
+output:
+
+
+*/
