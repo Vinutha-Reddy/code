76d1d0afc7fef25c6bba281cacc8fd96a4d817b1
Vivek Sharma <80456739+ifsvivek@users.noreply.github.com>
2024-02-15 04:18:16 +0000

    PAIN

--- c/OS/Lab/writer.c
+++ c/OS/Lab/writer.c
@@ -1,18 +1,17 @@
-#include<fcntl.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<stdio.h>
-int main()
-{
-    int fd;
-    char buf[1024];
-    char *myfifo="/tmp/myfifo";
-    mkfifo(myfifo,0666);
-    printf("Run reader process to read the FIFO file \n");
-    fd=open(myfifo,O_WRONLY);
-    write(fd,"Hello",sizeof("Hello"));
-    close(fd);
-    unlink(myfifo);
-    return 0;
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main() {
+  int fd;
+  char buf[1024];
+  char* myfifo = "/tmp/myfifo";
+  mkfifo(myfifo, 0666);
+  printf("Run reader process to read the FIFO file \n");
+  fd = open(myfifo, O_WRONLY);
+  write(fd, "Hello", sizeof("Hello"));
+  close(fd);
+  unlink(myfifo);
+  return 0;
 }
