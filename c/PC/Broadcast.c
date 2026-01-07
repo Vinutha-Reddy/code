529b79da998f0061a289cf65f1ec55b0c144a8c1
Vinutha <vinuthareddy47@gmail.com>
2026-01-07 19:00:14 +0530

    Initial Commit

---
+++ c/PC/Broadcast.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<mpi.h>
+int main(int argc, char** argv){
+    int rank,size;
+    int data;
+    MPI_Init(&argc,,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    if(rank==0){
+        data=42;
+        printf("Process %d broadcasting data = %d",rank,data);
+    }
+    MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Process %d received data = %d",rank,data);
+    MPI_Finalize();
+    return 0;
+}
